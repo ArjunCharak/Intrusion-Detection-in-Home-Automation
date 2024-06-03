7358955e3d66332b201a58c2086cf01f860b8a80
Arjun Charak <75723799+ArjunCharak@users.noreply.github.com>
2024-06-03 12:32:27 +0530

    cameraHumanDetection.py

--- Computervision Module/cameraHumanDetection.py
+++ Computervision Module/cameraHumanDetection.py
@@ -18,8 +18,7 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
               "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
               "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear"
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors"
               ]
 
 prev_frame_time = 0
